05bfbbd6c1feef3f09018b5601ccd8acb5d006c0
yultrish <yultrish@gmail.com>
2023-08-05 23:21:54 +0000

    restaurant

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('js is working')
+menuIcon = document.querySelector('.menu')
+closeIcon = document.querySelector('.close-icon')
+list = document.querySelector('ul')
+width = window.innerWidth;
+isClicked = false;
+
+function smallScreen(){
+    if (width<=899){
+         menuIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
+        list.style.display = 'block';
+        isClicked = true;
+    }    
+  return;
+}
+menuIcon.addEventListener('click', ()=>{
+    console.log('hi')
+       smallScreen()
+    
+})
+
+closeIcon.addEventListener('click', function(){
+    menuIcon.style.display = 'block';
+        closeIcon.style.display = 'none';
+        list.style.display = 'none';
+})
